ba6c3928813fd66df02709a92023543e73842648
Gfortes <mhxign@gmail.com>
2026-02-08 14:13:23 +0300

    build ready

--- apps/editor-desktop/vite.config.ts
+++ apps/editor-desktop/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "./", // ✅ важно для Electron file:// (иначе будет пустой экран)
   plugins: [react()],
   server: {
     port: 5173,
